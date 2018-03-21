74af2d1c1d160e604f90c6c0cf34cf42ea56ead9
Jose Luis Represa <joseluis.represa@bbva.com>
2018-03-21 20:31:10 +0100

    respond to ghibli

--- scripts/ghibli-api.js
+++ scripts/ghibli-api.js
@@ -6,7 +6,21 @@ module.exports = (robot) => {
   const adapter = robot.adapterName;
   const formatter = require(`../lib/formatters/${adapter}`);
 
-  robot.respond(/lista de peliculas/i, (res) => {
+
+  robot.hear(/ghibli/i, (res) => {
+    res.reply(`Hola humano!
+Si necesitas conocer más sobre alguna película de Ghibli creo que te puedo ser de ayuda.
+Los comandos que puedes utilizar son:
+
+- Para ver el listado de películas: "listado de peliculas"
+- Para ordenar el listado: "<n> <ultimas|primeras> peliculas"
+- Para ver el detalle de una película: "detalle de la película <id>"
+
+Un saludo!
+    `);
+  });
+
+  robot.respond(/listado de peliculas/i, (res) => {
     callAPI(robot, 'films').then((data) => {
       const message = formatter.formatFilms(data);
 
