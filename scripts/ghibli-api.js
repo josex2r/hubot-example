0ddf4a3f0783812cb7577194f3ed05eaa43f44f5
Jose Luis Represa <joseluis.represa@bbva.com>
2018-03-21 20:13:54 +0100

    order films/

--- scripts/ghibli-api.js
+++ scripts/ghibli-api.js
@@ -6,8 +6,29 @@ module.exports = (robot) => {
   const adapter = robot.adapterName;
   const formatter = require(`../lib/formatters/${adapter}`);
 
-  robot.respond(/lista de pel[ií]culas/i, (res) => {
+  robot.respond(/lista de peliculas/i, (res) => {
     callAPI(robot, 'films').then((data) => {
+      const message = formatter.formatFilms(data);
+
+      res.send(message);
+    }, (error) => {
+      res.reply(`Ha habido un error: ${error.message}`);
+    });
+  });
+
+  robot.respond(/(\d) (ultimas|primeras) peliculas/i, (res) => {
+    const limit = res.match[1];
+    const order = res.match[2] === 'ultimas' ? -1 : 1;
+
+    callAPI(robot, 'films').then((data) => {
+      data = data.sort((a, b) => a.release_date - b.release_date);
+
+      if (res.match[2] === 'ultimas') {
+        data = data.slice(-limit);
+      } else {
+        data = data.slice(0, limit);
+      }
+
       const message = formatter.formatFilms(data);
 
       res.send(message);
